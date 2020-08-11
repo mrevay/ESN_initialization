506d9eeeda519f7de57d346d7f849e4f7e038009
mrevay <maxrevay@gmail.com>
2020-08-11 16:16:21 +1000

    Added LREE as method of projecting onto stable matrices.

--- echo_state_network.py
+++ echo_state_network.py
@@ -12,7 +12,8 @@ from hyperopt import Trials
 
 class echo_state_network:
 
-    def __init__(self, n, m, p, q, connectivity, radius, phi, washout, bias_var=1, Bu_var=1, Du_var=1, ofb=True):
+    def __init__(self, n, m, p, q, connectivity, radius, phi, washout, bias_var=1,
+                 Bu_var=1, Du_var=1, ofb=True, gamma=1.0):
 
         self.n = int(n)
         self.m = int(m)
@@ -27,6 +28,8 @@ class echo_state_network:
 
         self.alpha = 0
         self.beta = 1
+
+        self.gamma = gamma
 
         # A bunch of hyperparameters
         self.bias_var = bias_var
@@ -58,8 +61,8 @@ class echo_state_network:
 
         self.A = np.zeros((self.n, self.n))
 
-        W = sp.sparse.random(self.n, self.n, self.connectivity) / np.sqrt(self.n)
-        # W = np.random.randn(self.n, self.n) / np.sqrt(self.n)
+        # W = sp.sparse.random(self.n, self.n, self.connectivity) / np.sqrt(self.n)
+        W = np.random.randn(self.n, self.n) / np.sqrt(self.n)
 
         eig = sp.sparse.linalg.eigs(W, 1)
         self.Bw = self.radius * W / np.abs(eig[0])
@@ -91,14 +94,18 @@ class echo_state_network:
         else:
             P = cvx.Variable((self.n, self.n), 'P', symmetric=True)
 
-        F = cvx.Variable((self.n, self.n), 'F')
+        # F11 = self.radius * np.random.randn(self.n // 2, self.n // 2) / np.sqrt(self.n / 2)
+        # zer = np.zeros((self.n // 2, self.n // 2))
+        # F = sp.block([[F11, zer], [zer, zer]])
+
+        F = np.zeros((self.n, self.n))
+        # F = cvx.Variable((self.n, self.n), 'F')
         # F = self.radius*np.random.randn(self.n, self.n) / np.sqrt(self.n)
 
-        # Bw = cvx.Variable((self.n, self.q), 'Bw')
-        Bw = np.zeros((self.n, self.n))
+        # Bw2 = cvx.Variable((self.n // 2, self.q), 'Bw')
+        # Bw = cvx.bmat([[zer, zer], [Bw2]])
+        Bw = cvx.Variable((self.n, self.q), 'Bw')
 
-        # Cv = np.random.randn(self.q, self.n) / np.sqrt(self.n)
-        # Cv = np.random.randn(self.n, self.n) / np.sqrt(self.n) + 1E-1 * np.eye(self.n)
         Cv = np.eye(self.n)
 
         Gamma_v = sp.linalg.block_diag(Cv, np.eye(self.q))
@@ -124,10 +131,16 @@ class echo_state_network:
         Ass = self.radius * np.random.randn(self.n, self.n,) / np.sqrt(self.n)
         self.Atild = Ass
 
+        Bss = self.radius * np.random.randn(self.n, self.n,) / np.sqrt(self.n)
+        self.Btild = Bss
+
         # objective = cvx.Minimize(cvx.norm(E @ Ass - F) + cvx.norm(Bw) + cvx.norm(Dw))
-        objective = cvx.Minimize(cvx.norm(E @ np.linalg.pinv(Cv) @ Ass - Bw - F))
+        # objective = cvx.Minimize(cvx.norm(E @ np.linalg.pinv(Cv) @ Ass - Bw - F))
         # objective = cvx.Minimize(cvx.norm(E @ Ass - Bw @ Cv - F))
         # objective = cvx.Minimize(cvx.norm(E @ Ass - Bw))
+
+        objective = cvx.Minimize(self.gamma * cvx.norm(E @ np.linalg.pinv(Cv) @ Ass - F) +
+                                 (1 - self.gamma) * cvx.norm(E @ np.linalg.pinv(Cv) @ Bss - Bw))
 
         prob = cvx.Problem(objective, constraints)
         prob.solve(solver=cvx.SCS, verbose=True)
@@ -148,8 +161,115 @@ class echo_state_network:
         else:
             self.Bw = Einv @ Bw.value
 
-        # self.Bw = Einv @ Bw.value
-        # self.Bw = Einv @ Bw
+        self.bias = self.bias_var * np.random.randn(self.q, 1)
+        self.Bu = self.Bu_var * np.random.randn(self.n, self.m)
+        self.Du = self.Du_var * np.random.randn(self.q, self.m)
+        self.Cv = Cv
+
+        self.P = P.value
+        self.E = E.value
+        self.multis = multis.value
+
+        # Add output feedback weight if necessary
+        if self.ofb:
+            self.Wofb = np.random.randn(self.n, self.p) / np.sqrt(self.p)
+
+    def ESN_init_LREE(self, diag_E=False, diag_P=False, sample=False, F_is_zero=False):
+
+        solver_tol = 1E-6
+        eps = 1E-3
+
+        multis = cvx.Variable((self.q), 'lambdas', nonneg=True)
+        T = cvx.diag(multis)
+
+        if diag_E:
+            E = cvx.diag(cvx.Variable((self.n, 1), 'E'))
+        else:
+            E = cvx.Variable((self.n, self.n), 'E')
+
+        if diag_P:
+            P = cvx.diag(cvx.Variable((self.n, 1), 'P'))
+        else:
+            P = cvx.Variable((self.n, self.n), 'P', symmetric=True)
+
+        if F_is_zero:
+            F = np.zeros((self.n, self.n))
+        else:
+            F = cvx.Variable((self.n, self.n), 'F')
+
+        Bw = cvx.Variable((self.n, self.q), 'Bw')
+
+        Cv = np.eye(self.n)
+
+        # Behavioural description of nonlinearity
+        Gamma_v = sp.linalg.block_diag(Cv, np.eye(self.q))
+        M = cvx.bmat([[-2 * self.alpha * self.beta * T, (self.alpha + self.beta) * T], [(self.alpha + self.beta) * T, - 2 * T]])
+
+        # Construct final LMI.
+        z1 = np.zeros((self.n, self.q))
+        z2 = np.zeros((self.q, self.q))
+
+        Mat11 = cvx.bmat([[E + E.T - P, z1], [z1.T, z2]]) - Gamma_v.T @ M @ Gamma_v
+        Mat21 = cvx.bmat([[F, Bw]])
+        Mat22 = P
+
+        Mat = cvx.bmat([[Mat11, Mat21.T], [Mat21, Mat22]])
+
+        # epsilon ensures strict feasability
+        constraints = [Mat >> (solver_tol) * np.eye(Mat.shape[0]),
+                       P >> (eps + solver_tol) * np.eye(self.n),
+                       E + E.T >> (eps + solver_tol) * np.eye(self.n)]
+
+        # Weight matrix to project onto stable set
+        Bss = self.radius * np.random.randn(self.n, self.n,) / np.sqrt(self.n)
+        self.Btild = Bss
+
+        if sample:
+            samples = 1000
+            xtild = 2 * np.random.randn(self.n, samples)
+            xtild_next = Bss @ self.phi(xtild)
+        else:
+            # Target distribution of eigenvalues.
+            xtild = np.eye(self.n)
+            xtild_next = Bss @ self.phi(xtild)
+
+        # Construct LREE
+        Phi = np.zeros((2*self.n+self.q, 2*self.n+self.q))
+        for ii in range(xtild.shape[1]):
+
+            # Construct data
+            wtild = self.phi(xtild[:, ii:ii+1])
+            zt = np.concatenate([xtild_next[:, ii:ii+1], xtild[:, ii:ii+1], wtild], 0)
+
+            Phi = Phi + zt @ zt.T
+
+        R = cvx.Variable((2*self.n + self.q, 2*self.n + self.q))
+
+        EFK = cvx.bmat([[E, -F, -Bw]])
+
+        Q = cvx.bmat([[R, EFK.T], [EFK, E + E.T - np.eye(self.n)]])
+        constraints.append(Q >> 0)
+
+        objective = cvx.Minimize(cvx.trace(Phi @ R))
+
+        prob = cvx.Problem(objective, constraints)
+        prob.solve(solver=cvx.SCS, verbose=True)
+        print("Initilization Status: ", prob.status)
+        print("Setup Time: ", prob.solver_stats.setup_time / 1000)
+        print("Solve Time: ", prob.solver_stats.solve_time / 1000)
+
+        # Initialize reservoir
+        Einv = np.linalg.inv(E.value)
+
+        if isinstance(F, np.ndarray):
+            self.A = Einv @ F
+        else:
+            self.A = Einv @ F.value
+
+        if isinstance(Bw, np.ndarray):
+            self.Bw = Einv @ Bw
+        else:
+            self.Bw = Einv @ Bw.value
 
         self.bias = self.bias_var * np.random.randn(self.q, 1)
         self.Bu = self.Bu_var * np.random.randn(self.n, self.m)
@@ -163,6 +283,42 @@ class echo_state_network:
         # Add output feedback weight if necessary
         if self.ofb:
             self.Wofb = np.random.randn(self.n, self.p) / np.sqrt(self.p)
+
+    def init_lin_driver(self, ofb=False):
+        # Initialize with a linear reservoir driving the nonlinear reservoir
+        solver_tol = 1E-6
+        eps = 1E-3
+
+        F11 = self.radius * np.random.randn(self.n // 2, self.n // 2) / np.sqrt(self.n / 2)
+        zer = np.zeros((self.n // 2, self.n // 2))
+        F = sp.block([[F11, zer], [zer, zer]])
+
+        Bw22 = self.radius * np.random.randn(self.n // 2, self.n // 2) / np.sqrt(self.n // 2)
+
+        Bw21 = np.random.randn(self.n // 2, self.n // 2)
+        Bw = sp.block([[zer, zer], [Bw21, Bw22]])
+
+        Cv = np.eye(self.n)
+
+        if isinstance(F, np.ndarray):
+            self.A = F
+        else:
+            self.A = F.value
+
+        if isinstance(Bw, np.ndarray):
+            self.Bw = Bw
+        else:
+            self.Bw = Bw.value
+
+        self.bias = self.bias_var * np.random.randn(self.q, 1)
+        self.Bu = self.Bu_var * np.random.randn(self.n, self.m)
+        self.Du = self.Du_var * np.random.randn(self.q, self.m)
+        self.Cv = Cv
+
+        # Add output feedback weight if necessary
+        if self.ofb:
+            self.Wofb = np.random.randn(self.n, self.p) / np.sqrt(self.p)
+
 
     def train(self, data):
 
@@ -184,6 +340,32 @@ class echo_state_network:
         Ytilde = y[:, self.washout:]
         Xtilde = X[:, self.washout:]
         self.Cy = Ytilde @ np.linalg.pinv(Xtilde)
+
+    def train_stable(self, data):
+
+        u = data["u"]
+        y = data["y"]
+
+        #Simulate ESN dynamics on training data
+        X = np.zeros((self.n, y.shape[1]))
+        for t in range(1, y.shape[1]):
+            vt = self.Cv @ X[:, t-1:t] + self.Du @ u[:, t:t+1] + self.bias
+            wt = self.phi(vt)
+
+            if self.ofb:
+                X[:, t:t+1] = self.A @ X[:, t-1:t] + self.Bw @ wt + \
+                              self.Bu @ u[:, t:t+1] + self.Wofb @ y[:, t-1:t]
+            else:
+                X[:, t:t+1] = self.A @ X[:, t-1:t] + self.Bw @ wt + self.Bu @ u[:, t:t+1]
+
+        Ytilde = y[:, self.washout:]
+        Xtilde = X[:, self.washout:]
+
+        multis = cvx.Variable(self.q, 1)
+        Cy = cvx.Variable(self.p, self.n)
+        J = cvx.norm(Ytilde - Cy @ Xtilde) ** 2
+
+
 
     def test(self, data):
 
@@ -222,9 +404,10 @@ if __name__ == "__main__":
     param_search = False
 
     def phi(x):
-        # return np.maximum(x, 0)
-        return np.tanh(x)
-        # return x
+        # return np.maximum(x, 0)  # ReLU
+        return np.tanh(x)  
+        # return np.sin(np.pi * x) / (np.pi * x)  # Sinc
+        # return x   # Linear
 
     # [train, val, test] = loader.load_data(dataset='WH')
     [train, val, test] = loader.load_data(dataset='Silverbox')
@@ -271,11 +454,14 @@ if __name__ == "__main__":
     Bu_var = 1.0
     ESN = echo_state_network(n, 1, 1, q, 1.0, radius, phi, 200,
                              bias_var=bias_var, Du_var=Du_var,
-                             Bu_var=Bu_var)
+                             Bu_var=Bu_var, ofb=False, gamma=0)
 
 
     # Initialize the reservoir
-    ESN.ESN_init_IEE(diag_E=False, diag_P=False)
+    # ESN.ESN_init_IEE(diag_E=False, diag_P=False)
+    ESN.ESN_init_LREE(diag_E=False, diag_P=True, sample=True, F_is_zero=True)
+
+    # ESN.init_lin_driver()
     # ESN.ESN_init_sr()
     # ESN.ESN_init_orthog(radius=0.9)
 
@@ -301,7 +487,7 @@ if __name__ == "__main__":
     eigs = np.linalg.eigvals(ESN.A + ESN.Bw)
     plt.plot(np.real(eigs), np.imag(eigs), 'o')
 
-    # eigs = np.linalg.eigvals(ESN.Atild)
+    # eigs = np.linalg.eigvals(ESN.Btild)
     # plt.plot(np.real(eigs), np.imag(eigs), 's')
 
     plt.legend(['B eigs', 'A eigs',  'A + B eigs', 'Atild'])
